3d1e3be6d7c66d8b0af2e79a7cb1bb759bd60614
DrozdovNikolai <112856511+DrozdovNikolai@users.noreply.github.com>
2022-10-10 16:13:44 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,17 +4,14 @@ window.addEventListener("DOMContentLoaded", function (event) {
         let f1 = document.getElementById("field1").value;
         let f2 = document.getElementById("field2").value;
         let r = document.getElementById("result");
-        console.log(f1);
         let onlydigitnotzero=/^(?!(0))\d+$/;
         if(f1.match(onlydigitnotzero)===null || f2.match(onlydigitnotzero)===null)
         {
           alert("Введено не число или начинается с нуля");
           r.innerHTML="Ошибка NaN";
-            
         }
         else
         {
-
           r.innerHTML = parseInt(f1) * parseInt(f2);
         }
       }
