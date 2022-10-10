d381171a3542f35542dfe477b62377748fc7ff05
DrozdovNikolai <112856511+DrozdovNikolai@users.noreply.github.com>
2022-10-10 16:11:32 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-
-window.addEventListener('DOMContentLoaded', function (event) {
+window.addEventListener("DOMContentLoaded", function (event) {
     console.log("DOM fully loaded and parsed");
     function click1() {
-        let f1 = document.getElementsByName("field1");
-        let f2 = document.getElementsByName("field2");
+        let f1 = document.getElementById("field1").value;
+        let f2 = document.getElementById("field2").value;
         let r = document.getElementById("result");
-
-        if(isNaN(parseInt(f1[0].value)) || isNaN(parseInt(f2[0].value)))
+        console.log(f1);
+        let onlydigitnotzero=/^(?!(0))\d+$/;
+        if(f1.match(onlydigitnotzero)===null || f2.match(onlydigitnotzero)===null)
         {
-            alert("Введено не число")
-
-            r.innerHTML="Ошибка NaN";
-
+          alert("Введено не число или начинается с нуля");
+          r.innerHTML="Ошибка NaN";
+            
         }
-        else r.innerHTML = parseInt(f1[0].value) * parseInt(f2[0].value);
+        else
+        {
+
+          r.innerHTML = parseInt(f1) * parseInt(f2);
+        }
       }
       let b = document.getElementById("button1");
       b.addEventListener("click", click1);
