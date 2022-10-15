8887cd5d5db9df29b6ab198d1ffadf4992e183bc
DrozdovNikolai <112856511+DrozdovNikolai@users.noreply.github.com>
2022-10-16 00:25:27 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,127 @@
-window.addEventListener("DOMContentLoaded", function (event) {
-    console.log("DOM fully loaded and parsed");
-    function click1() {
-        let f1 = document.getElementById("field1").value;
-        let f2 = document.getElementById("field2").value;
-        let r = document.getElementById("result");
-        let onlydigitnotzero=/^(?!(0))\d+$/;
-        if(f1.match(onlydigitnotzero)===null || f2.match(onlydigitnotzero)===null)
-        {
-          alert("Введено не число или начинается с нуля");
-          r.innerHTML="Ошибка NaN";
-        }
-        else
-        {
-          r.innerHTML = parseInt(f1) * parseInt(f2);
-        }
+function updatePrice(v) {
+  
+  let s = document.getElementsByName("prodType");
+  let select = s[0];
+
+  // Скрываем или показываем радиокнопки.
+  let radioDiv = document.getElementById("prodOptions");
+  radioDiv.style.display = (select.value == "1" || select.value == "3" ? "none" : "block");
+
+
+
+  // Скрываем или показываем чекбоксы.
+  let checkDiv = document.getElementById("checkboxes");
+  checkDiv.style.display = (select.value == "3" ? "block" : "none");
+  let price = 0;
+
+  let prices = getPrices();
+  let priceIndex = parseInt(select.value) - 1;
+  if (priceIndex >= 0) {
+    price = prices.prodTypes[priceIndex];
+  }
+
+  // Смотрим какая товарная опция выбрана.
+  if(select.value == "2" ){
+  let radios = document.getElementsByName("prodOptions");
+  radios.forEach(function (radio) {
+    if (radio.checked) {
+      let optionPrice = prices.prodOptions[radio.value];
+      if (optionPrice !== undefined) {
+        price += optionPrice;
       }
-      let b = document.getElementById("button1");
-      b.addEventListener("click", click1);
+    }
+  });
+}
+if(select.value == "3" ){
+  let checkboxes= document.getElementsByName("checkboxes");
+  checkboxes.forEach(function (checkboxes) {
+    if (checkboxes.checked) {
+      let checkboxPrice = prices.prodProperties[checkboxes.value];
+      if (checkboxPrice !== undefined) {
+        price += checkboxPrice;
+      }
+    }
+  });
+}
+
+ let onlydigitnotzero=/^(?!(0))\d+$/;
+ let prodPrice = document.getElementById("result");
+ if(v.match(onlydigitnotzero)===null)
+ prodPrice.innerHTML = "Число не введено или начинается с нуля";
+else{
+
+  prodPrice.innerHTML = v * price + " рублей";
+}
+}
+
+
+function getPrices() {
+  return {
+    prodTypes: [100, 200, 300],
+    prodOptions: {
+      option2: 5,
+      option3: 10,
+    },
+    prodProperties: {
+      check1: 133,
+      check2: 277,
+    }
+  };
+}
+
+
+
+
+window.addEventListener("DOMContentLoaded", function (event) {
+  let radioDiv = document.getElementById("prodOptions");
+  radioDiv.style.display = "none";
+
+  let checkboxDiv = document.getElementById("checkboxes");
+  checkboxDiv.style.display = "none";
+  console.log("DOM fully loaded and parsed");
+
+
+  let v = document.getElementById("field");
+  v.addEventListener("input", function (event) {
+    console.log(v.value);
+    updatePrice(v.value);
+  });
+
+  // Находим select по имени в DOM.
+  let s = document.getElementsByName("prodType");
+  let select = s[0];
+  // Назначаем обработчик на изменение select.
+  select.addEventListener("change", function (event) {
+    let target = event.target;
+    console.log(target.value);
+    updatePrice(v.value);
+  });
+
+  // Назначаем обработчик радиокнопок.  
+  let radios = document.getElementsByName("prodOptions");
+  radios.forEach(function (radio) {
+    radio.addEventListener("change", function (event) {
+      let r = event.target;
+      console.log(r.value);
+      updatePrice(v.value);
     });
+  });
+
+
+
+  // Назначаем обработчик радиокнопок.  
+  let checkboxes = document.getElementsByName("checkboxes");
+  
+  checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener("change", function (event) {
+      console.log(checkboxes);
+      let c = event.target;
+      console.log(c.name);
+      console.log(c.value);
+      updatePrice(v.value);
+    });
+  });
+
+});
 
 
